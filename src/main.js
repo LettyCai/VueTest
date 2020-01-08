d6de02a2dfa5579bfd8dad2b1b706e9ce5a8e09b
LettyCai <sei_clj@126.com>
2020-01-08 16:56:01 +0800

    20200108

--- src/main.js
+++ src/main.js
@@ -31,6 +31,13 @@ import VueResource from 'vue-resource'
 //2.2安装vue-resource
 Vue.use(VueResource)
 
+//导入mui scroll 控件
+import  mui from './lib/mui/js/mui.js'
+//初始化scroll控件
+mui('.mui-scroll-wrapper').scroll({
+	deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+});
+
 
 import app from './App.vue'
 
--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ import SearchContainer from './components/tabbar/SearchContainer.vue'
 import MemberContainer from './components/tabbar/MemberContainer.vue'
 import NewsList from './components/home/NewsList.vue'
 import NewsInfo from './components/home/NewsInfo.vue'
+import pictures from './components/home/pictures.vue'
 
 
 // 3. 创建路由对象
@@ -17,7 +18,8 @@ var router = new VueRouter({
     { path:'/Member',component:MemberContainer},
     { path:'/',redirect:'/home'},
     { path:'/home/newslist',component:NewsList},
-    { path:'/home/newsinfo:id',component:NewsInfo}
+    { path:'/home/newsinfo:id',component:NewsInfo},
+    { path:'/home/pictures',component:pictures}
   ],
   linkActiveClass:'mui-active'  //覆盖默认的路由高亮类 router-link-active
 })
